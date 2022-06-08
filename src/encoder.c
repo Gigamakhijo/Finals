9dbc69d0ab81fa1c49b47ab77a3a0d978819f079
urw7rs <urw.7rs@icloud.com>
2022-06-08 19:56:30 +0900

    feat:#4 add protoypes and implement ReadText

--- src/encoder.c
+++ src/encoder.c
@@ -1,3 +1,21 @@
-#include "bit.h"
+#include <stdio.h>
 
-int main() { return 0; }
+#include "text.h"
+
+int main(int argc, char *argv[]) {
+  Data data;
+
+  FILE *fp = fopen(argv[1], "r");
+  if (!fp) {
+    perror("File opening failed");
+    return -1;
+  }
+
+  printf("%d\n", ReadText(&data, fp));
+
+  fclose(fp);
+  // encode();
+  // compress();
+  // add_ecc();
+  // write_bits();
+}
