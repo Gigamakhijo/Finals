9b0f27c794d5faa735d32fc33c0249ccec3211ce
urw7rs <urw.7rs@icloud.com>
2022-06-10 22:57:31 +0900

    feat:10 fix encoder writes fix decoder id

--- src/decoder.c
+++ src/decoder.c
@@ -17,7 +17,7 @@ int main() {
 
   char c;
 
-  int GENDER_count;
+  int GENDER_count = 0;
 
   int ITEM_count;
   int ITEM_name;
@@ -54,17 +54,23 @@ int main() {
     strcpy(user.gender, "FEMALE");
 
   bread(INT, bf, &user.age, 7, 1);
+  printf("%d\n", user.age);
   bread(INT, bf, &user.hp, 8, 1);
+  printf("%d\n", user.hp);
   bread(INT, bf, &user.mp, 8, 1);
+  printf("%d\n", user.mp);
   bread(INT, bf, &user.coin, 16, 1);
+  printf("%d\n", user.coin);
 
   bread(INT, bf, &ITEM_count, 3, 1); // ITEM 종류 개수
+  printf("%d\n", ITEM_count);
 
   user.items = new_queue();
 
   for (int k = 0; k < ITEM_count; k++) {
     item = new_item();
     bread(INT, bf, &ITEM_name, 3, 1);
+    printf("%d\n", ITEM_count);
     if (ITEM_name == 0)
       strcpy(item->name, "BOMB");
     else if (ITEM_name == 1)
@@ -77,96 +83,105 @@ int main() {
       strcpy(item->name, "SHILED");
     else
       strcpy(item->name, "CANNON");
-
+    printf("%s\n", item->name);
     bread(INT, bf, &item->count, 8, 1);
-
+    printf("%d\n", ITEM_count);
     enqueue(user.items, item);
   }
 
   bread(INT, bf, &FRIEND_count, 7, 1); //친구 수
+  printf("%d\n", FRIEND_count);
 
   i = 0;
-
   user.friends = new_queue();
+  printf("friend count : %d\n", FRIEND_count);
   for (int k = 0; FRIEND_count; k++) {
+
     friend = new_friend();
+    i = 0;
+    while (1) {
+      bread(CHAR, bf, &c, 7, 1);
+      if (c == ',') {
+        friend->id[i] = '\0';
+        break;
+      } else {
+        friend->id[i++] = c;
+      }
+    }
+    printf("id : %s\n", friend->id);
+    i = 0;
+
     while (1) {
       bread(CHAR, bf, &c, 7, 1);
       if (c == ',')
         break;
       else {
-        friend->id[i] = c;
-        i++;
-      }
-
-      i = 0;
-      //
-      while (1) {
-        bread(CHAR, bf, &c, 7, 1);
-        if (c == ',')
-          break;
-        else {
-          friend->name[i] = c;
-          i++;
-        }
-      }
-      bread(CHAR, bf, &GENDER_count, 1, 1);
-      if (GENDER_count == 0)
-        strcpy(friend->gender, "MALE");
-      else
-        strcpy(friend->gender, "FEMALE");
-      bread(INT, bf, &friend->age, 7, 1);
-
-      enqueue(user.friends, friend);
-    }
-
-    i = 0;
-
-    while (1) {
-      bread(CHAR, bf, &c, 7, 1);
-      if (c == '\0')
-        break;
-      else {
-        user.description[i] = c;
+        friend->name[i] = c;
         i++;
       }
     }
+    printf("%s\n", friend->name);
+    bread(CHAR, bf, &GENDER_count, 1, 1);
+    printf("%c\n", c);
+    printf("gender cound %d\n", GENDER_count);
+    if (GENDER_count == 0)
+      strcpy(friend->gender, "MALE");
+    else
+      strcpy(friend->gender, "FEMALE");
+    bread(INT, bf, &friend->age, 7, 1);
+    printf("%s\n", friend->gender);
+    printf("%d\n", friend->age);
 
-    printf("%s\n", user.id);
-    printf("%s\n", user.name);
-    printf("%s\n", user.gender);
-    printf("%d\n", user.age);
-    printf("%d\n", user.hp);
-    printf("%d\n", user.mp);
-    printf("%d\n", user.coin);
-    printf("%d\n\n", ITEM_count);
-
-    for (int k = 0; k < ITEM_count; k++) {
-      Item *item = dequeue(user.items);
-      printf("%s : ", item->name);
-      printf("%d\n", item->count);
-    }
-
-    printf("\n%d\n", FRIEND_count);
-
-    for (int k = 0; k < FRIEND_count; k++) {
-      Friend *friend = dequeue(user.friends);
-      printf("%s / ", friend->id);
-      printf("%s / ", friend->name);
-      printf("%s / ", friend->gender);
-      printf("%d / ", friend->age);
-      printf("\n\n");
-    }
-
-    printf("%s\n", user.description);
-
-    // write_text();
-
-    free_queue(user.items);
-    free_queue(user.friends);
-    free_item(item);
-    free_friend(friend);
-
-    bclose(bf);
+    enqueue(user.friends, friend);
   }
+
+  // read description
+  i = 0;
+  while (1) {
+    bread(CHAR, bf, &c, 7, 1);
+    printf("%c", c);
+    if (c == '\0')
+      break;
+    else {
+      user.description[i] = c;
+      i++;
+    }
+  }
+
+  printf("%s\n", user.id);
+  printf("%s\n", user.name);
+  printf("%s\n", user.gender);
+  printf("%d\n", user.age);
+  printf("%d\n", user.hp);
+  printf("%d\n", user.mp);
+  printf("%d\n", user.coin);
+  printf("%d\n\n", ITEM_count);
+
+  for (int k = 0; k < ITEM_count; k++) {
+    Item *item = dequeue(user.items);
+    printf("%s : ", item->name);
+    printf("%d\n", item->count);
+  }
+
+  printf("\n%d\n", FRIEND_count);
+
+  for (int k = 0; k < FRIEND_count; k++) {
+    Friend *friend = dequeue(user.friends);
+    printf("%s / ", friend->id);
+    printf("%s / ", friend->name);
+    printf("%s / ", friend->gender);
+    printf("%d / ", friend->age);
+    printf("\n\n");
+  }
+
+  printf("%s\n", user.description);
+
+  // write_text();
+
+  free_queue(user.items);
+  free_queue(user.friends);
+  free_item(item);
+  free_friend(friend);
+
+  bclose(bf);
 }
--- src/encoder.c
+++ src/encoder.c
@@ -24,40 +24,40 @@ int main(int argc, char *argv[]) {
 
   char next = ',';
   int g_buffer;
-  int fg_buffer;
   int item_len;
   int friend_len;
   int i_buffer;
 
-  char *str_male = "MALE";
-  char *str_female = "FEMALE";
+  const char str_male[] = "MALE";
+  const char str_female[] = "FEMALE";
   if (strcmp(user.gender, str_male) == 0)
     g_buffer = 0;
   else if (strcmp(user.gender, str_female) == 0)
     g_buffer = 1;
-	printf("id : %s length : %ld\n", user.id, strlen(user.id));
-	printf("name : %s length : %ld\n", user.name, strlen(user.name));
+
   bwrite(CHAR, bf, user.id, 7, strlen(user.id));
   bwrite(CHAR, bf, &next, 7, 1);
- 	bwrite(CHAR, bf, user.name, 7, strlen(user.name));
+  bwrite(CHAR, bf, user.name, 7, strlen(user.name));
   bwrite(CHAR, bf, &next, 7, 1);
   bwrite(INT, bf, &g_buffer, 1, 1);
   bwrite(INT, bf, &user.age, 7, 1);
   bwrite(INT, bf, &user.hp, 8, 1);
   bwrite(INT, bf, &user.mp, 8, 1);
   bwrite(INT, bf, &user.coin, 16, 1);
-  char *bomb = "BOMB";
-  char *potion = "POTION";
-  char *cure = "CURE";
-  char *book = "BOOK";
-  char *shield = "SHIELD";
-  char *cannon = "CANNON";
+
+  char *bomb = "BOMB: ";
+  char *potion = "POTION: ";
+  char *cure = "CURE: ";
+  char *book = "BOOK: ";
+  char *shield = "SHIELD: ";
+  char *cannon = "CANNON: ";
 
   item_len = q_len(user.items);
   bwrite(INT, bf, &item_len, 3, 1);
 
   for (int i = 0; i < item_len; i++) {
     item = dequeue(user.items);
+    printf("%s\n", item->name);
     if (strcmp(item->name, bomb) == 0)
       i_buffer = 0;
     else if (strcmp(item->name, potion) == 0)
@@ -75,28 +75,28 @@ int main(int argc, char *argv[]) {
     bwrite(INT, bf, &item->count, 8, 1);
   }
 
+  int gender;
   friend_len = q_len(user.friends);
   bwrite(INT, bf, &friend_len, 7, 1);
   for (int i = 0; i < friend_len; i++) {
     friend = dequeue(user.friends);
 
     bwrite(CHAR, bf, friend->id, 7, strlen(friend->id));
+    printf("%d : %s\n", i, friend->id);
     bwrite(CHAR, bf, &next, 7, 1);
     bwrite(CHAR, bf, friend->name, 7, strlen(friend->name));
     bwrite(CHAR, bf, &next, 7, 1);
     if (strcmp(friend->gender, str_male) == 0) {
-      fg_buffer = 0;
+      gender = 0;
     } else if (strcmp(friend->gender, str_female) == 0) {
-      fg_buffer = 1;
+      gender = 1;
     }
-    bwrite(INT, bf, &fg_buffer, 1, 1);
+    bwrite(INT, bf, &gender, 1, 1);
     bwrite(INT, bf, &friend->age, 7, 1);
   }
-	printf("%s\n", user.description);
-	printf("\nlength : %ld", strlen(user.description));
+
   bwrite(CHAR, bf, user.description, 7, strlen(user.description));
-	bflush(bf);
-	bclose(bf);
+
   free_queue(user.items);
   free_queue(user.friends);
 }
