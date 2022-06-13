c6d138e24d12659872d8ddb9652c185606bfccd3
ChoiYongWon <yongwon0824@naver.com>
2022-06-13 11:27:57 +0900

    feat: #12 removed unnecessary file

--- src/decoder.c
+++ src/decoder.c
@@ -4,5 +4,8 @@
 int main(int argc, char *argv[]) {
 	revert_fivot(argv[1], "decoder.fivot");
   	revert_correction("decoder.fivot", "decoder.correction");
-  	revert_protocol("decoder.correction", "decoder.protocol");
+  	revert_protocol("decoder.correction", "decode.txt");
+
+	remove("decoder.fivot");
+	remove("decoder.correction");
 }
--- src/encoder.c
+++ src/encoder.c
@@ -5,4 +5,8 @@ int main(int argc, char *argv[]) {
   convert_protocol(argv[1], "encoder.protocol");
   convert_correction("encoder.protocol", "encoder.correction");
   convert_fivot("encoder.correction", "encoder.fivot");
+
+  remove("encoder.protocol");
+  remove("encoder.correction");
+  rename("encoder.fivot", "encoded_data");
 }
